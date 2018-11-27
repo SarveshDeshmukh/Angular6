875dfb43966acb8f7d1f8abfdbaae511a3d47994
Sarvesh Deshmukh <sarvesh_deshmukh@yahoo.com>
2018-11-27 02:29:51 -0500

    assignment two and three

--- Assignment1/src/app/app.module.ts
+++ Assignment1/src/app/app.module.ts
@@ -1,18 +1,24 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import { FormsModule } from '@angular/forms';
 
 import { AppComponent } from './app.component';
 import { SucessAlertComponent } from './sucess-alert/sucess-alert.component';
 import { FailureAlertComponent } from './failure-alert/failure-alert.component';
+import { AssignmentTwoComponent } from './assignment-two/assignment-two.component';
+import { AssignmentThreeComponent } from './assignment-three/assignment-three.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     SucessAlertComponent,
     FailureAlertComponent,
+    AssignmentTwoComponent,
+    AssignmentThreeComponent,
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    FormsModule,
   ],
   providers: [],
   bootstrap: [AppComponent]
---
+++ Assignment1/src/app/assignment-two/assignment-two.component.ts
@@ -0,0 +1,19 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-assignment-two',
+  templateUrl: './assignment-two.component.html',
+  styleUrls: ['./assignment-two.component.css']
+})
+export class AssignmentTwoComponent implements OnInit {
+  username = 'Initial';
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+  onResetUsername() {
+    this.username = '';
+  }
+
+}
